13af3d5cdb9fa8e0d88c1ed0481da3376268d797
deviveirosmel <deviveirosmelissa@gmail.com>
2025-05-23 12:41:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function openWin() {
+    window.open("https://www.canva.com/design/DAGmy1FLVyE/hYY8eT-m0WUMZL4EujxufQ/view?utm_content=DAGmy1FLVyE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hc6cd54cc6b");
+  }
